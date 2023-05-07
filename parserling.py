ba2ee9918085af33f9d85ef1765d3e11a66b76fd
Henrique Martinelli Frezzatti <henriquemf@al.insper.edu.br>
2023-05-07 11:35:33 -0300

    Melhoras, ainda com erros

--- parserling.py
+++ parserling.py
@@ -1,74 +1,104 @@
 from rply import ParserGenerator
-from lexerling import lgfinal
 
-pg = ParserGenerator(['START_PROGRAM', 'PROGRAM_END', 'IDENTIFIER', 'COMMA', 'NAIL_IDENTIFIER', 'CAUSES', 
+class Parser():
+    def __init__(self):
+        self.pg = ParserGenerator(['START_PROGRAM', 'PROGRAM_END', 'IDENTIFIER', 'COMMA', 'NAIL_IDENTIFIER', 'CAUSES', 
                       'DMG_TYPE', 'BEING', 'DIGIT', 'TO', 'ENTITY_TYPE', 'SPELL_IDENTIFIER', 'AND', 
                       'CONDITIONAL', 'CONDITIONAL_END', 'IF', 'LIFE_IS', 'RESULTS_IN', 'LIFE_CONDITION', 'DATA_TYPE', 'VARIABLES', 'LIVES_AS',
                       'VARIABLES_DECLARATION', 'VARIABLES_END', 'FUNCTION', 'ENTITIES', 'PURE_VESSEL', 'SEALED_VESSEL',
                       'GAME_LOOP', 'GAME_LOOP_END','ACTION', 'WITH',
                       ])
 
-@pg.production('program : START_PROGRAM statement_list PROGRAM_END')
-def program(p):
-    return p[1]
+    def parse(self):
+        @self.pg.production('program : START_PROGRAM statement_list PROGRAM_END')
+        def program(p):
+            print('teste')
+            return p[1]
 
-@pg.production('statement_list : statement')
-def statement_list_single(p):
-    return [p[0]]
+        @self.pg.production('statement_list : statement')
+        def statement_list_single(p):
+            print('teste1')
+            return [p[0]]
 
-@pg.production('statement_list : statement statement_list')
-def statement_list_multiple(p):
-    return [p[0]] + p[1]
+        @self.pg.production('statement_list : statement statement_list')
+        def statement_list_multiple(p):
+            print('teste2')
+            return [p[0]] + p[1]
 
-@pg.production('statement : conditional')
-@pg.production('statement : variables_declaration')
-@pg.production('statement : function')
-@pg.production('statement : game_loop')
-@pg.production('statement : action')
-def statement(p):
-    return p[0]
+        @self.pg.production('statement : conditional')
+        @self.pg.production('statement : variables_declaration')
+        @self.pg.production('statement : function')
+        @self.pg.production('statement : game_loop')
+        @self.pg.production('statement : action')
+        def statement(p):
+            print('teste3')
+            return p[0]
 
-@pg.production('identifier_list : IDENTIFIER')
-def identifier_list_single(p):
-    return [p[0].getstr()]
+        @self.pg.production('identifier_list : IDENTIFIER')
+        def identifier_list_single(p):
+            print('teste4')
+            return [p[0].getstr()]
 
-@pg.production('identifier_list : IDENTIFIER COMMA identifier_list')
-def identifier_list_multiple(p):
-    return [p[0].getstr()] + p[2]
+        @self.pg.production('identifier_list : IDENTIFIER COMMA identifier_list')
+        def identifier_list_multiple(p):
+            print('teste5')
+            return [p[0].getstr()] + p[2]
 
-@pg.production('damage_causing_condition : NAIL_IDENTIFIER CAUSES DMG_TYPE BEING DIGIT TO ENTITY_TYPE')
-@pg.production('damage_causing_condition : SPELL_IDENTIFIER CAUSES DMG_TYPE BEING DIGIT TO ENTITY_TYPE')
-@pg.production('damage_causing_condition : NAIL_IDENTIFIER CAUSES DMG_TYPE BEING DIGIT TO ENTITY_TYPE AND life_check')
-@pg.production('damage_causing_condition : SPELL_IDENTIFIER CAUSES DMG_TYPE BEING DIGIT TO ENTITY_TYPE AND life_check')
-def damage_causing_condition(p):
-    return p
+        @self.pg.production('damage_causing_condition : NAIL_IDENTIFIER CAUSES DMG_TYPE BEING DIGIT TO ENTITY_TYPE')
+        @self.pg.production('damage_causing_condition : SPELL_IDENTIFIER CAUSES DMG_TYPE BEING DIGIT TO ENTITY_TYPE')
+        @self.pg.production('damage_causing_condition : NAIL_IDENTIFIER CAUSES DMG_TYPE BEING DIGIT TO ENTITY_TYPE AND life_check')
+        @self.pg.production('damage_causing_condition : SPELL_IDENTIFIER CAUSES DMG_TYPE BEING DIGIT TO ENTITY_TYPE AND life_check')
+        def damage_causing_condition(p):
+            print('teste6')
+            return p
 
-@pg.production('conditional : CONDITIONAL damage_causing_condition CONDITIONAL_END')
-def conditional(p):
-    return p[1]
+        @self.pg.production('conditional : CONDITIONAL damage_causing_condition CONDITIONAL_END')
+        def conditional(p):
+            print('teste7')
+            return p[1]
 
-@pg.production('life_check : IF IDENTIFIER LIFE_IS DIGIT RESULTS_IN LIFE_CONDITION')
-def life_check(p):
-    return p
+        @self.pg.production('life_check : IF IDENTIFIER LIFE_IS DIGIT RESULTS_IN LIFE_CONDITION')
+        def life_check(p):
+            print('teste8')
+            return p
+        
+        @self.pg.production('variables : DATA_TYPE VARIABLES IDENTIFIER LIVES_AS IDENTIFIER')
+        def variables(p):
+            print('teste9')
+            return p
 
-@pg.production('variables : DATA_TYPE VARIABLES IDENTIFIER LIVES_AS IDENTIFIER')
-def variables(p):
-    return p
+        @self.pg.production('variables_list : variables')
+        def variables_list_single(p):
+            return [p[0]]
 
-@pg.production('variables_declaration : VARIABLES_DECLARATION variables VARIABLES_END')
-def variables_declaration(p):
-    return p
+        @self.pg.production('variables_list : variables variables_list')
+        def variables_list_multiple(p):
+            print('teste10')
+            return [p[0]] + p[1]
 
-@pg.production('function : FUNCTION IDENTIFIER ENTITIES identifier_list PURE_VESSEL statement_list SEALED_VESSEL')
-def function(p):
-    return p
+        @self.pg.production('variables_declaration : VARIABLES_DECLARATION variables_list VARIABLES_END')
+        def variables_declaration(p):
+            print('teste10')
+            return p
 
-@pg.production('game_loop : GAME_LOOP statement_list GAME_LOOP_END')
-def game_loop(p):
-    return p
+        @self.pg.production('function : FUNCTION IDENTIFIER ENTITIES identifier_list PURE_VESSEL statement_list SEALED_VESSEL')
+        def function(p):
+            print('teste11')
+            return p
 
-@pg.production('action : ACTION IDENTIFIER WITH identifier_list')
-def action(p):
-    return p
+        @self.pg.production('game_loop : GAME_LOOP statement_list GAME_LOOP_END')
+        def game_loop(p):
+            print('teste12')
+            return p
 
-parser = pg.build()
+        @self.pg.production('action : ACTION IDENTIFIER WITH identifier_list')
+        def action(p):
+            print('teste13')
+            return p
+
+        @self.pg.error
+        def error_handle(token):
+            raise ValueError(token)
+
+    def get_parser(self):
+        return self.pg.build()
--- test.py
+++ test.py
@@ -1,16 +1,16 @@
-from lexerling import lgfinal
-from parserling import parser
+from lexerling import Lexer
+from parserling import Parser
 from rply.errors import ParsingError
 
 text = """
 Hollownest is infected
 souls to be sacrificed
-npc: npc1 lives as Iselda
-npc: npc2 lives as Cornifer
-player: hollowknight lives as HollowKnight
-boss: boss1 lives as FalseKnight
-enemy: enemy1 lives as Aspid
-enemy: enemy2 lives as Boulder
+npc:npc1 lives as Iselda
+npc:npc2 lives as Cornifer
+player:hollowknight lives as HollowKnight
+boss:boss1 lives as FalseKnight
+enemy:enemy1 lives as Aspid
+enemy:enemy2 lives as Boulder
 they became the void
 full soul
 purenail causes playerdmg being 21 to enemy1 & if enemy1 life is less than 21 results in dead
@@ -18,24 +18,11 @@ empty soul
 You became the void
 """
 
-tokens = lgfinal.lex(text)
-tokens = list(tokens)
 
-def token_generator(token_list):
-    for token in token_list:
-        yield token
+lexer = Lexer().get_lexer()
+tokens = lexer.lex(text)
 
-# Imprima os tokens gerados
-print("Tokens gerados:")
-for token in tokens:
-    print(token)
-
-for i in range(1, len(tokens) + 1):
-    partial_tokens = tokens[:i]
-    try:
-        result = parser.parse(token_generator(partial_tokens))
-        print(f"Parsing successful up to token {i}: {tokens[i-1]}")
-    except Exception as e:
-        print(f"Parsing failed at token {i}: {tokens[i-1]}")
-        print("Erro de análise sintática:", e)
-        break
+pg = Parser()
+pg.parse()
+parser = pg.get_parser()
+parser.parse(tokens)
